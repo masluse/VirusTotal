2fe3b69ba7f2d88338cd2ad7a90ddfb2e9c0c0b6
masluse <122784119+masluse@users.noreply.github.com>
2023-06-01 16:49:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
-import os
-import datetime
+from flask import Flask, request, render_template, send_from_directory
 import requests
-from flask import Flask, request, render_template, render_template_string, send_from_directory
+import os
+import json
+from datetime import datetime
+from jinja2 import Environment, FileSystemLoader
 
 app = Flask(__name__)
-API_KEY = 'your-api-key'
+API_KEY = 'your_virustotal_public_api_key'
+BASE_URL = 'https://www.virustotal.com/vtapi/v2/file/report'
 UPLOAD_DIR = 'uploads'
 
-@app.route('/')
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+env = Environment(loader=FileSystemLoader('templates'))
+
+def check_hash(hash_value):
+    params = {'apikey': API_KEY, 'resource': hash_value}
+    response = requests.get(BASE_URL, params=params)
+    return response.json()
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        hashes = request.form['hashes'].splitlines()
+        results = []
+        for hash_value in hashes:
+            result = check_hash(hash_value)
+            if result['response_code']:
+                results.append((hash_value, result['positives'], result['total']))
+            else:
+                results.append((hash_value, 'Not found', 'Not found'))
+        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        filename = f'{timestamp}.html'
+        with open(os.path.join(UPLOAD_DIR, filename), 'w') as f:
+            result_template = env.get_template('result.html')
+            html = result_template.render(results=results)
+            f.write(html)
+        return render_template('result.html', filename=filename, results=results)
     files = os.listdir(UPLOAD_DIR)
     return render_template('index.html', files=files)
 
@@ -16,48 +44,5 @@ def index():
 def uploaded_file(filename):
     return send_from_directory(UPLOAD_DIR, filename)
 
-@app.route('/', methods=['GET', 'POST'])
-def check_hashes():
-    if request.method == 'POST':
-        hashes = request.form.get('hashes').splitlines()
-        results = []
-        for h in hashes:
-            response = requests.get(f'https://www.virustotal.com/vtapi/v2/file/report?apikey={API_KEY}&resource={h}')
-            json_response = response.json()
-            if json_response['response_code']:
-                results.append([h, json_response['positives'], json_response['total']])
-            else:
-                results.append([h, 'Not found'])
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        filename_html = f'{timestamp}.html'
-        html_content = render_template_string('''<!DOCTYPE html>
-        <html>
-        <head>
-            <title>Result</title>
-        </head>
-        <body>
-            <h1>Result</h1>
-            <table>
-                <tr>
-                    <th>Hash</th>
-                    <th>Malicious</th>
-                </tr>
-                {% for result in results %}
-                <tr>
-                    <td>{{ result[0] }}</td>
-                    <td>{% if result[1] != 'Not found' %}{{ result[1] }} / {{ result[2] }}{% else %}Not found{% endif %}</td>
-                </tr>
-                {% endfor %}
-            </table>
-            <a href="/">Back</a>
-        </body>
-        </html>''', results=results)
-        with open(os.path.join(UPLOAD_DIR, filename_html), 'w') as f:
-            f.write(html_content)
-        return render_template('result.html', results=results)
-    return render_template('index.html')
-
 if __name__ == '__main__':
-    if not os.path.exists(UPLOAD_DIR):
-        os.makedirs(UPLOAD_DIR)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
