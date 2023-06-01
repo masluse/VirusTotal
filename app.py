9acba0bddf9e4feac2f325c5e5772ea51dca9ac8
Manuel Regli <manuel@familie-regli.ch>
2023-06-01 18:20:03 +0000

    Main

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import os
 import json
 from datetime import datetime
 import time
-from threading import Lock
-import concurrent.futures
+import threading
 from jinja2 import Environment, FileSystemLoader
 
 app = Flask(__name__)
@@ -13,37 +12,38 @@ API_KEY = os.environ.get('VT_API_KEY', 'your_default_virustotal_public_api_key')
 BASE_URL = 'https://www.virustotal.com/vtapi/v2/file/report'
 UPLOAD_DIR = 'uploads'
 WAIT_TIME = 15  # 15 seconds wait time
-lock = Lock()  # to block simultaneous requests
 
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 env = Environment(loader=FileSystemLoader('templates'))
 
-def check_hash(hash_value):
+def check_hash(hash_value, results):
     params = {'apikey': API_KEY, 'resource': hash_value}
     response = requests.get(BASE_URL, params=params)
     time.sleep(WAIT_TIME)
-    return response.json()
+    result = response.json()
+    if result.get('response_code'):
+        results.append((hash_value, result.get('positives', 'Not found'), result.get('total', 'Not found')))
+    else:
+        results.append((hash_value, 'Not found', 'Not found'))
+
+def process_hashes(hashes):
+    results = []
+    for hash_value in hashes:
+        check_hash(hash_value, results)
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    filename = f'{timestamp}.html'
+    with open(os.path.join(UPLOAD_DIR, filename), 'w') as f:
+        result_template = env.get_template('result.html')
+        html = result_template.render(results=results)
+        f.write(html)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        with lock:
-            hashes = request.form['hashes'].splitlines()
-            results = []
-            for hash_value in hashes:
-                result = check_hash(hash_value)
-                if result.get('response_code'):
-                    results.append((hash_value, result.get('positives', 'Not found'), result.get('total', 'Not found')))
-                else:
-                    results.append((hash_value, 'Not found', 'Not found'))
-            timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-            filename = f'{timestamp}.html'
-            with open(os.path.join(UPLOAD_DIR, filename), 'w') as f:
-                result_template = env.get_template('result.html')
-                html = result_template.render(results=results)
-                f.write(html)
-            return render_template('result.html', filename=filename, results=results, wait_time=WAIT_TIME)
+        hashes = request.form['hashes'].splitlines()
+        threading.Thread(target=process_hashes, args=(hashes,)).start()
+        return "Your hashes are being processed. Please check back later."
     files = os.listdir(UPLOAD_DIR)
     return render_template('index.html', files=files)
 
